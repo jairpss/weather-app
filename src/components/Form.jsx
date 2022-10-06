73aaf8a139068a81c44eb435bad244d3da6e6875
Jair Partida <jairpartida1@gmail.com>
2022-10-05 20:47:52 -0600

    Form ui done

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,7 +2,36 @@ import React from 'react'
 
 const Form = () => {
   return (
-    <div>Form</div>
+    <div className="container">
+        <form>
+            <div className="field">
+                <label htmlFor="city">City</label>
+                <input 
+                    type="text"
+                    id="city"
+                    name="city"
+                />
+            </div>
+
+            <div className="field">
+                <label htmlFor="country">Country</label>
+                <select id="country" name="country">
+                    <option value="">--Select a country--</option>
+                    <option value="US">United States</option>
+                    <option value="MX">Mexico</option>
+                    <option value="AR">Argentina</option>
+                    <option value="CO">Colombia</option>
+                    <option value="ES">España</option>
+                    <option value="PE">Peru</option>
+                </select>
+            </div>
+
+            <input 
+                type="submit"
+                value="Get weather"
+            />
+        </form>
+    </div>
   )
 }
 
